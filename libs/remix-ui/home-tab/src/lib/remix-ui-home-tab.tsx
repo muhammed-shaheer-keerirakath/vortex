3ecb36c0cefa34e15c8faeb25d4295b60dfaee0f
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2024-12-09 12:45:23 +0400

    fix: removes home tab learn

--- libs/remix-ui/home-tab/src/lib/remix-ui-home-tab.tsx
+++ libs/remix-ui/home-tab/src/lib/remix-ui-home-tab.tsx
@@ -1,11 +1,9 @@
-import React, {useState, useEffect, useContext} from 'react' // eslint-disable-line
+import React, { useState, useEffect, useContext } from 'react' // eslint-disable-line
 
 import './remix-ui-home-tab.css'
 import { ThemeContext, themes } from './themeContext'
 import HomeTabTitle from './components/homeTabTitle'
 import HomeTabFile from './components/homeTabFile'
-import HomeTabLearn from './components/homeTabLearn'
-import HomeTabScamAlert from './components/homeTabScamAlert'
 import HomeTabGetStarted from './components/homeTabGetStarted'
 import HomeTabFeatured from './components/homeTabFeatured'
 import HomeTabFeaturedPlugins from './components/homeTabFeaturedPlugins'
@@ -28,9 +26,9 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
   const { plugin } = props
 
   const [state, setState] = useState<{
-    themeQuality: {filter: string; name: string}
+    themeQuality: { filter: string; name: string }
   }>({
-    themeQuality: themes.light
+    themeQuality: themes.light,
   })
   const [carouselWidth, setCarouselWidth] = useState(65)
 
@@ -40,7 +38,7 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
       setState((prevState) => {
         return {
           ...prevState,
-          themeQuality: theme.quality === 'dark' ? themes.dark : themes.light
+          themeQuality: theme.quality === 'dark' ? themes.dark : themes.light,
         }
       })
     })
@@ -49,7 +47,7 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
       setState((prevState) => {
         return {
           ...prevState,
-          themeQuality: theme.quality === 'dark' ? themes.dark : themes.light
+          themeQuality: theme.quality === 'dark' ? themes.dark : themes.light,
         }
       })
     })
@@ -71,7 +69,6 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
     }
   }, [])
 
-  //  border-right
   return (
     <div className="d-flex flex-column w-100" data-id="remixUIHTAll">
       <ThemeContext.Provider value={state.themeQuality}>
@@ -79,13 +76,10 @@ export const RemixUiHomeTab = (props: RemixUiHomeTabProps) => {
           <div className="px-2 pl-3 justify-content-start border-right d-flex flex-column" id="remixUIHTLeft" style={{ width: 'inherit' }}>
             <HomeTabTitle />
             <HomeTabGetStarted plugin={plugin}></HomeTabGetStarted>
-            {!(platform === appPlatformTypes.desktop) ?
-              <HomeTabFile plugin={plugin} />:
-              <HomeTabFileElectron plugin={plugin}></HomeTabFileElectron>}
-            {/* <HomeTabLearn plugin={plugin} /> */}
+            {!(platform === appPlatformTypes.desktop) ? <HomeTabFile plugin={plugin} /> : <HomeTabFileElectron plugin={plugin}></HomeTabFileElectron>}
           </div>
           <div className="pl-2 pr-3 justify-content-start d-flex flex-column" style={{ width: `${carouselWidth}%` }} id="remixUIHTRight">
-            <LanguageOptions plugin={plugin}/>
+            <LanguageOptions plugin={plugin} />
             <HomeTabFeatured plugin={plugin}></HomeTabFeatured>
             <HomeTabFeaturedPlugins plugin={plugin}></HomeTabFeaturedPlugins>
           </div>
