c130a69d7d9bcae2a734c94a31c9ee1204e7f887
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2024-12-10 17:07:50 +0400

    fix: removes ethers from scripts lib

--- libs/remix-ws-templates/src/templates/remixDefault/index.ts
+++ libs/remix-ws-templates/src/templates/remixDefault/index.ts
@@ -7,11 +7,7 @@ export default async () => {
     // @ts-ignore
     'contracts/3_MyToken.sol': (await import('raw-loader!./contracts/3_MyToken.sol')).default,
     // @ts-ignore
-    'scripts/deploy_with_ethers.ts': (await import('!!raw-loader!./scripts/deploy_with_ethers.ts')).default,
-    // @ts-ignore
     'scripts/deploy_with_web3.ts': (await import('!!raw-loader!./scripts/deploy_with_web3.ts')).default,
-    // @ts-ignore
-    'scripts/ethers-lib.ts': (await import('!!raw-loader!./scripts/ethers-lib.ts')).default,
     // @ts-ignore
     'scripts/web3-lib.ts': (await import('!!raw-loader!./scripts/web3-lib.ts')).default,
     // @ts-ignore
