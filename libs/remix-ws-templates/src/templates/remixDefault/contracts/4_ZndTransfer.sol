aa2d4df79631550e9214222f4079fa3b53366802
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2024-12-24 14:54:48 +0400

    feat: updates znd transfer solidity

--- libs/remix-ws-templates/src/templates/remixDefault/contracts/4_ZndTransfer.sol
+++ libs/remix-ws-templates/src/templates/remixDefault/contracts/4_ZndTransfer.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 contract ZndTransfer {
     // Event to log Znd transfers
-    event ZndSent(address indexed to, uint256 amount);
     event ZndReceived(address indexed from, uint256 amount);
 
     // Function to receive Znd directly into the contract
@@ -14,15 +13,5 @@ contract ZndTransfer {
     // Function to view the contract's Znd balance
     function getBalance() public view returns (uint256) {
         return address(this).balance;
-    }
-
-    // Function to send Znd to a specific address
-    function sendZnd(address payable recipient, uint256 amount) public {
-        require(
-            address(this).balance >= amount,
-            "Not enough Znd in the contract"
-        );
-        recipient.transfer(amount);
-        emit ZndSent(recipient, amount);
     }
 }
